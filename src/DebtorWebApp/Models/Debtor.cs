113a7576b6aac1d9b1c44edb09f4fbae0d0e3e28
Carlos Jimenez <developer@cmjimenez.com>
2019-09-14 01:50:02 -0400

    add debtor and invoice models

---
+++ src/DebtorWebApp/Models/Debtor.cs
@@ -0,0 +1,22 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using IbanNet.DataAnnotations;
+
+namespace DebtorWebApp.Models
+{
+    public class Debtor
+    {
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        public string FirstName { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        public string LastName { get; set; }
+        [RegularExpression(@"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-0-9a-z]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$")]
+        [Required]
+        public string Email { get; set; }
+        [Iban]
+        public string IBAN { get; set; }
+    }
+}
---
+++ src/DebtorWebApp/Models/Invoice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DebtorWebApp.Models
+{
+    public class Invoice
+    {
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+        [Required]
+        public InvoiceStatus Status {get; set;}
+    }
+
+    public enum InvoiceStatus
+    {
+        Open,
+        Paid,
+        Overdue
+    }
+}
