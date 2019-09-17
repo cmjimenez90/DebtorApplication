ae2b2ab68a8bc41112b0e17d9d26f3a0424123c0
Carlos Jimenez <developer@cmjimenez.com>
2019-09-16 20:46:50 -0400

    add basic page for debtor list and create - no styling

---
+++ src/DebtorWebApp/Pages/Debtors/CreateDebtor.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DebtorWebApp.Data;
+using DebtorWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DebtorWebApp.Pages.Debtors
+{
+    public class NewDebtorModel : PageModel
+    {
+        private readonly ApplicationDbContext context;
+
+        [BindProperty]
+        public Debtor Debtor {get; set;}
+        
+        public NewDebtorModel(ApplicationDbContext context) => this.context = context;
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            context.Debtors.Add(Debtor);
+            await context.SaveChangesAsync();
+
+            return RedirectToPage("./index");
+        }
+    }
+}
