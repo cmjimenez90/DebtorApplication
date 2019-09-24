999604a272e47063bc05329686baddb03e5fd995
Carlos Jimenez <developer@cmjimenez.com>
2019-09-23 22:35:58 -0400

    data migrations for authorization

--- src/DebtorWebApp/Data/Migrations/20190920002145_AddOwners.Designer.cs
+++ src/DebtorWebApp/Data/Migrations/20190920002145_AddOwners.Designer.cs
@@ -4,14 +4,16 @@ using DebtorWebApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DebtorWebApp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190920002145_AddOwners")]
+    partial class AddOwners
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
