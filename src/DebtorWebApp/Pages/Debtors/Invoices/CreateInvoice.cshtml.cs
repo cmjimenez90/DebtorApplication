b5dcc0a71e9f26263a78bb47df6a3c967ba140d0
Carlos M Jimenez <developer@cmjimenez.com>
2019-12-29 02:59:02 -0500

    Add Invoice Creation

---
+++ src/DebtorWebApp/Pages/Debtors/Invoices/CreateInvoice.cshtml.cs
@@ -0,0 +1,42 @@
+﻿
+using System.Threading.Tasks;
+using DebtorWebApp.Data;
+using DebtorWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DebtorWebApp.Pages.Debtors.Invoices
+{
+    public class CreateInvoiceModel : BasePageModel
+    {
+        public Invoice Invoice { get; set; }
+        public CreateInvoiceModel(ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<IdentityUser> userManager) : base(context, authorizationService, userManager)
+        {
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id,[Bind("Amount,DueDate")]Invoice invoice) {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            Debtor existingDebtor = await Context.Debtors.FirstOrDefaultAsync(debtor => debtor.DebtorID == id);
+            if (null == existingDebtor) return NotFound();
+
+            invoice.DebtorID = existingDebtor.DebtorID;
+            invoice.Status = InvoiceStatus.Open;
+
+            Context.Invocies.Add(invoice);
+            await Context.SaveChangesAsync();
+            return RedirectToPage("/Debtors/ViewDebtor", new { id });
+        }
+    }
+}
---
+++ src/DebtorWebApp/Pages/Debtors/Invoices/ViewInvoice.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DebtorWebApp.Data;
+using DebtorWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DebtorWebApp.Pages.Debtors.Invoices
+{
+    public class ViewInvoiceModel : BasePageModel
+    {
+        public Invoice Invoice { get; set; }
+        public string InvoiceOwnerName { get; set; }
+        public ViewInvoiceModel(ApplicationDbContext context, IAuthorizationService authorizationService, UserManager<IdentityUser> userManager) : base(context, authorizationService, userManager)
+        {
+        }
+
+        public IActionResult OnGet(int invoiceID)
+        {
+           Invoice = Context.Invocies.FirstOrDefault(invoice => invoice.InvoiceId == invoiceID);
+           if(Invoice == null)
+            {
+                return NotFound();
+            }
+            Debtor owner = Context.Debtors.FirstOrDefault(debtor => debtor.DebtorID == Invoice.DebtorID);
+            InvoiceOwnerName = (owner.LastName + ", " + owner.FirstName);
+            return Page();
+        }
+    }
+}
